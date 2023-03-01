b8bd30b58b87f61b956430bd45a565439b4dbb84
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-03-01 08:46:04 +0100

    initial commit

---
+++ components/layout/Header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import classes from "./Header.module.css";
+import { useRouter } from "next/router";
+
+function Header() {
+  const router = useRouter();
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about" activeClassName="active">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/Blogs">Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
