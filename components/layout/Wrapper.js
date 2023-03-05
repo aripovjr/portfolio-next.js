87962c1ccbde2a7767f5cb45c16128da74adcd0b
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-03-05 08:52:17 +0100

    form for admin panel to post blogs

---
+++ components/layout/Wrapper.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+const Wrapper = styled.section`
+  margin: 1rem 0;
+  padding: 2rem;
+  background-color: #f4f5fa;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 7px;
+  box-shadow: 0px 0px 27px -17px rgba(66, 68, 90, 1);
+`;
+
+import React from "react";
+
+function WrapperContainer(props) {
+  return <Wrapper>{props.children}</Wrapper>;
+}
+
+export default WrapperContainer;
